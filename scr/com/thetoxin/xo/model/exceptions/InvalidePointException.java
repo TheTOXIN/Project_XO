e848fb0f005ce099f5b08f19927cf7af5361c88a
TheTOXIN <thetoksin@mail.ru>
2017-01-08 15:30:16 +0300

    +InvalidePointException +XOException

---
+++ scr/com/thetoxin/xo/model/exceptions/InvalidePointException.java
@@ -0,0 +1,7 @@
+package com.thetoxin.xo.model.exceptions;
+
+public class InvalidePointException extends XOException {
+
+
+
+}
---
+++ scr/com/thetoxin/xo/model/exceptions/XOException.java
@@ -0,0 +1,5 @@
+package com.thetoxin.xo.model.exceptions;
+
+public abstract class XOException extends Exception {
+
+}
