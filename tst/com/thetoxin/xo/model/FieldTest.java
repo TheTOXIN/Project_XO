69c2e55b42f019fda4096755408a9b69bf5bdac3
TheTOXIN <thetoksin@mail.ru>
2017-01-08 15:19:41 +0300

    +FieldTest

---
+++ tst/com/thetoxin/xo/model/FieldTest.java
@@ -0,0 +1,23 @@
+package com.thetoxin.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+    @Test
+    public void getSize() throws Exception {
+
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+
+    }
+
+    @Test
+    public void setFigure() throws Exception {
+
+    }
+
+}
