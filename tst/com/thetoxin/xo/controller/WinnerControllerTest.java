051a0967c80db681b3523957d9e378b3aadc48f5
TheTOXIN <thetoksin@mail.ru>
2017-01-10 19:36:29 +0300

    fixes bugs for WinnerController +WinnerControllerTest

---
+++ tst/com/thetoxin/xo/controller/WinnerControllerTest.java
@@ -0,0 +1,96 @@
+package com.thetoxin.xo.controller;
+
+import com.thetoxin.xo.model.Field;
+import com.thetoxin.xo.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class WinnerControllerTest {
+    @Test
+    public void getWinnerWhenRow() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.X);
+            field.setFigure(new Point(i, 2), Figure.X);
+            assertEquals(Figure.X, winnerController.getWinner(field));
+        }
+    }
+    @Test
+    public void getWinnerNoWhenRow() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.X);
+            field.setFigure(new Point(i, 2), Figure.O);
+            assertNull(winnerController.getWinner(field));
+        }
+    }
+    @Test
+    public void getWinnerWhenColumn() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.X);
+            assertEquals(Figure.X, winnerController.getWinner(field));
+        }
+    }
+    @Test
+    public void getWinnerWhenNoColumn() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.O);
+            assertNull(winnerController.getWinner(field));
+        }
+    }
+    @Test
+    public void getWinnerWhenDiogonal1() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.X);
+        assertEquals(Figure.X, winnerController.getWinner(field));
+    }
+
+    @Test
+    public void getWinnerWhenNoDiogonal1() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.O);
+        assertNull(winnerController.getWinner(field));
+    }
+    @Test
+    public void getWinnerWhenDiogonal2() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.X);
+        assertEquals(Figure.X, winnerController.getWinner(field));
+    }
+
+    @Test
+    public void getWinnerWhenNoDiogonal2() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+        assertNull(winnerController.getWinner(field));
+    }
+
+
+}
